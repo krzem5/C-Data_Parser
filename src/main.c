ff9a7c7739b33ec6959495182bcf617204ad1183
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-04-09 22:26:34 +0200

    Push Update 09/04/2023, 22:26:34

--- src/main.c
+++ src/main.c
@@ -4,5 +4,14 @@
 
 
 int main(int argc,const char*const* argv){
+	int a;
+	float b;
+	char c[256];
+	unsigned int d;
+	if (parse_data("Abc (text) -48 1.89 'The quick brown fox'",41,"Abc (%4?) %d %f '%s'",&a,&b,c,&d)){
+		c[d]=0;
+		printf("[%d], [%f], [%s]\n",a,b,c);
+	}
+	printf("Q\n");
 	return 0;
 }
